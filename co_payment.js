190b44902af5a3bf671c0a8d978e5b98b7a6e6fb
Nicholas Le <nle031@west-mec.org>
2019-04-18 17:22:01 -0700

    Final Commit
    Broke my repository so had to start over.

--- co_payment.js
+++ co_payment.js
@@ -7,8 +7,8 @@
 
    Payment Form Script
    
-   Author: 
-   Date:   
+   Author: Nicholas Le
+   Date:   04/11/19
    
    Filename: co_payment.js
    
@@ -43,22 +43,203 @@
       Returns true of idNum satisfies the Luhn Algorithm
 
 */
+window.addEventListener("load", function () {
+      //retrieve the filed/value pairs from the url
+      var formData = location.search.slice(1);
+
+      formData = formData.replace(/\+/g, " ");
+
+      formData = decodeURIComponent(formData);
+
+      var formFields = formData.split(/[&=1]/g);
+      //write the field values to the order form
+      document.forms.order.elements.orderDate.value = formFields[1];
+
+      document.forms.order.elements.modelName.value = formFields[5];
+
+      document.forms.order.elements.qty.value = formFields[7];
+
+      document.forms.order.elements.initialCost.value = formFields[9];
+
+      document.forms.order.elements.protectionName.value = formFields[13];
+
+      document.forms.order.elements.protectionCost.value = formFields[15];
+
+      document.forms.order.elements.subtotal.value = formFields[17];
+
+      document.forms.order.elements.salesTax.value = formFields[19];
+
+      document.forms.order.elements.totalCost.value = formFields[21];
+});
+
 
 window.addEventListener("load", function () {
-   //retrieve the field/value pairs from the URL
-   var formData = location.search.slice(1);
-   formData = formData.replace(/\+/g, " ");
-   formData = decodeURIComponent(formData);
-   var formFields = formData.split(/[&=]/g);
 
-   //write the field values to the order form
-   document.forms.order.elements.orderDate.value = formFields[1];
-   document.forms.order.elements.modelName.value = formFields[5];
-   document.forms.order.elements.qty.value = formFields[7];
-   document.forms.order.elements.initialCost.value = formFields[9];
-   document.forms.order.elements.protectionName.value = formFields[13];
-   document.forms.order.elements.protectionCost.value = formFields[15];
-   document.forms.order.elements.subtotal.value = formFields[17];
-   document.forms.order.elements.salesTax.value = formFields[19];
-   document.forms.order.elements.totalCost.value = formFields[21];
+      document.getElementById("subButton").onclick = runSubmit;
+
+      document.getElementById("cardName").oninput = validateName;
+
+      document.getElementById("cardNumber").oninput = validateNumber;
+
+      document.getElementById("expMonth").onchange = validateMonth;
+
+      document.getElementById("expYear").onchange = validateYear;
+
+      document.getElementById("cvc").oninput = validateCVC;
 });
+
+
+function runSubmit() {
+
+      validateName();
+
+      validateCredit();
+
+      validateMonth();
+
+      validateYear();
+
+      validateCVC();
+}
+
+
+function validateMonth() {
+
+      var cardMonth = document.getElementById("expMonth");
+
+      if (cardMonth.selectedIndex === 0) {
+
+            cardMonth.setCustomValidity("Select the expiration month.");
+
+      } else {
+
+            cardMonth.setCustomValidity("");
+      }
+}
+
+function validateCredit() {
+
+      var creditCard = document.forms.payment.elements.credit[0];
+
+      if (creditCard.validity.valueMissing) {
+
+            creditCard.setCustomValidity("Select your credit card");
+
+      } else {
+
+            creditCard.setCustomValidity("");
+      }
+}
+
+function validateName() {
+
+      var cardName = document.getElementById("cardName");
+
+      if (cardName.validity.valueMissing) {
+
+            cardName.setCustomValidity("Enter your name as it apears on your card");
+
+      } else {
+
+            cardName.setCustomValidity("");
+      }
+}
+
+function validateNumber() {
+
+      var cardNumber = document.getElementById("cardNumber");
+
+      if (cardNumber.validity.valueMissing) {
+
+            cardNumber.setCustomValidity("Enter your card Number")
+
+      } else if (cardNumber.validity.patternMisMatch) {
+
+            cardNumber.setCustomValidity("Enter a valid card Number")
+
+      } else if (luhn(cardNumber.value) === false) {
+
+            cardNumber.setCustomValidity("Enter a legitimant card number")
+
+      } else {
+
+            creditCard.setCustomValidity("")
+      }
+}
+
+
+function validateCVC() {
+
+      var cardCVC = document.getElementById("cvc")
+
+      var creditCard = document.querySelector('input[name="credit"]:checked').value;
+
+      if (cardCVC.validity.valueMissing) {
+
+            setCustomValidity("Enter your CVC number")
+
+      } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
+
+            cardCVC.setCustomValidity("Enter a 4-digit CVC number");
+
+      } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
+
+            cardCVC.setCustomValidity("Enter a 3-digit CVC number");
+
+      } else {
+
+            cardCVC.setCustomValidity("");
+      }
+}
+
+function validateYear() {
+
+      var cardYear = document.getElementById("expYear")
+
+      if (cardYear.selectedIndex === 0) {
+
+            cardYear.setCustomValidity("Select the expiration year.");
+
+      } else {
+
+            cardYear.setCustomValidity("");
+
+
+      }
+}
+
+function sumDigits(numStr) {
+
+      var digitTotal = 0;
+
+      for (let i = 0; i < numStr.length; i++) {
+
+            digitTotal += parseInt(numStr.charAt(i));
+
+      }
+
+      return digitTotal;
+
+}
+
+function luhn(idNum) {
+
+      var string1 = "";
+
+      var string2 = "";
+      //retrive the odd number dig||its
+      for (let i = idNum.length - 1; i >= 0; i -= 2) {
+
+            string1 += idNum.charAt(i);
+
+      }
+      //retirve even number and double them
+      for (let i = idNum.length - 2; i >= 0; i -= 2) {
+
+            string2 += 2 * idNum.charAt(i);
+
+      }
+      //return wether the sum of the digits is divisible by 10
+      return sumDigits(string1 + string2) % 10 === 0;
+
+}
